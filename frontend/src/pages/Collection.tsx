c57850f1518ece34b47e0f7b0ce5216ada347467
TechnologicNick <technologicnick@gmail.com>
2022-04-01 19:19:11 +0200

    Add statistics

--- frontend/src/pages/Collection.tsx
+++ frontend/src/pages/Collection.tsx
@@ -1,13 +1,31 @@
 import { useState } from "react";
-import { Container, Pagination, Row, Text } from "@nextui-org/react";
+import { Container, Pagination, Row, Text, Button, Col, Spacer } from "@nextui-org/react";
 import { MdVerified } from "react-icons/md";
 import DisplayGrid from "../components/DisplayGrid";
 import Nft from "../models/nft";
 import images from "../resources/images.json";
 import { imagesPerPage } from "../settings";
+import { FaEthereum } from "react-icons/fa";
 
 const nfts = images.map(img => new Nft(img))
     .sort((a, b) => b.price - a.price)
+
+const priceSum = nfts.map(nft => nft.price).reduce((a, b) => a + b);
+
+const Stat = ({ title, subtitle }: { title: any, subtitle: any }) => {
+    return (
+        <Button css={{ height: "auto" }}>
+            <Col>
+                <Row justify="center" align="center">
+                    <Text size={20} b>{title}</Text>
+                </Row>
+                <Row justify="center" css={{ height: 32 }}>
+                    <Text size={14}>{subtitle}</Text>
+                </Row>            
+            </Col>
+        </Button>
+    )
+}
 
 const Collection = () => {
     const [page, setPage] = useState(0);
@@ -19,7 +37,26 @@ const Collection = () => {
             </Row>
             <Row justify="center" align="center">
                 <Text h5 color="$gray400">Created by <Text as="span" color="warning">The Farmer</Text></Text>
-                <MdVerified color="var(--nextui-colors-warning)" style={{ marginInline: "0.5ch" }}/>
+                <MdVerified color="var(--nextui-colors-warning)" style={{ marginLeft: "0.5ch" }}/>
+            </Row>
+            <Spacer y={1} />
+            <Row justify="center" align="center">
+                <Button.Group color="warning" ghost>
+                    <Stat title={nfts.length} subtitle="items" />
+                    <Stat title={Math.round(nfts.length / 1.8 + Math.random() * 10)} subtitle="owners" />
+                    <Stat title={(
+                        <Row justify="center" align="center">
+                            <FaEthereum />
+                            {nfts[nfts.length - 1].price.toFixed(3)}
+                        </Row>
+                    )} subtitle="floor price" />
+                    <Stat title={(
+                        <Row justify="center" align="center">
+                            <FaEthereum />
+                            {((priceSum * (11.22 + Math.random())) / 1000).toFixed(1) + "K"}
+                        </Row>
+                    )} subtitle="volume traded" />
+                </Button.Group>
             </Row>
         </Container>
         <Container fluid>
